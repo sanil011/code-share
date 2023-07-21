9179cd1d6e691b946410f4cb93c8cae756d74c57
sanil011 <sanilp191@gmail.com>
2023-07-21 20:50:16 +0530

    add links

--- client/src/components/socket.tsx
+++ client/src/components/socket.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 import * as io from "socket.io-client";
 
 export const Socket = async() => {
-    return (io.connect('http://localhost:3001'))
+    return (io.connect('https://code-server-r8cq.onrender.com/'))
 }
 
